e44bdc1439c4f0f75cd1e0a05c30c1138038609e
ryanTavcar <ryan.tavcar@gmail.com>
2021-03-16 12:15:59 +1100

    :recycle: Change Code Around in Product Show page

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,9 +1,5 @@
 module ProductsHelper
 
-    def purchased_false(product)
-      current_user && product.purchased == false
-    end
-
     def format_likes(product)
       product.likes.count == 1 ? 'Like' : 'Likes'
     end
