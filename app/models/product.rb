466e74cd6d72710d4b3706779297a319e1aab198
ryanTavcar <ryan.tavcar@gmail.com>
2021-03-20 14:34:04 +1100

    filaise for deployment

--- app/models/product.rb
+++ app/models/product.rb
@@ -24,7 +24,7 @@ class Product < ApplicationRecord
 
   def remove_whitespace
     self.name = self.name.strip
-    self.description = self.biography.strip
+    self.description = self.description.strip
   end
 
 end
